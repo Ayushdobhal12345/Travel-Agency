fe065bc8b1dddc97bf68b7ac98d670294f8ce20d
ayush dobhal <ayushdobhal12345@gmail.com>
2025-01-21 23:56:02 +0530

    commit

--- Travel-Agency/src/App.jsx
+++ Travel-Agency/src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
 import Slider from "react-slick";
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faTwitter, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from 'react-icons/fa'; // Importing FontAwesome icons
@@ -10,7 +9,30 @@ import { faPhoneAlt, faEnvelope, faMapMarkerAlt } from '@fortawesome/free-solid-
 import { saveUserData, saveBookingData } from './firebase'; // handle popup and booking form
 
 import logo from './assets/Logo.png';
+import callIcon from './assets/phone.jpg'; // Your call icon
+import PhoneIcon from './assets/phone.webp'; // Your call icon for header
+import whatsappIcon from './assets/watsaap.png'; // Your WhatsApp icon
+import emailIcon from './assets/mail.webp'; // Email icon (example)
+import instaIcon from './assets/insta.webp'; // Instagram icon (example)
+
+
+import heroImage2 from './assets/badrinath.jpg'; // Example hero image
+import heroImage3 from './assets/kedarnath.jpg'; // Example hero image
+import heroImage4 from './assets/8905891.jpg'; // Example hero image
+import heroImage5 from './assets/9378822.jpg'; // Example hero image
+
 import Whychoose from './assets/Whychoose.png';
+
+import service1 from './assets/Flights.jpg'; // Example image
+import service2 from './assets/Train.jpg'; // Example image
+import service3 from './assets/Bus.jpg'; // Example image
+import service4 from './assets/Hotel & Stay.jpg'; // Example image
+import service5 from './assets/Travel Visa.jpg'; // Example image
+import service6 from './assets/Travel Insurance.jpg'; // Example image
+import service7 from './assets/Passport.jpg'; // Example image
+import service8 from './assets/Holiday Vaccations.jpg'; // Example image
+
+
 import destination1 from './assets/Baku.jpg'; // Example image
 import destination2 from './assets/Bali.jpg'; // Example image
 import destination3 from './assets/Thailand.jpg'; // Example image
@@ -26,26 +48,6 @@ import destination12 from './assets/Andaman.jpg'; // Example image
 import destination13 from './assets/Kerala.jpg'; // Example image
 
 
-import service1 from './assets/Flights.jpg'; // Example image
-import service2 from './assets/Train.jpg'; // Example image
-import service3 from './assets/Bus.jpg'; // Example image
-import service4 from './assets/Hotel & Stay.jpg'; // Example image
-import service5 from './assets/Travel Visa.jpg'; // Example image
-import service6 from './assets/Travel Insurance.jpg'; // Example image
-import service7 from './assets/Passport.jpg'; // Example image
-import service8 from './assets/Holiday Vaccations.jpg'; // Example image
-
-
-import heroImage2 from './assets/badrinath.jpg'; // Example hero image
-import heroImage3 from './assets/kedarnath.jpg'; // Example hero image
-import heroImage4 from './assets/8905891.jpg'; // Example hero image
-import heroImage5 from './assets/9378822.jpg'; // Example hero image
-import callIcon from './assets/phone.jpg'; // Your call icon
-import PhoneIcon from './assets/phone.webp'; // Your call icon for header
-import whatsappIcon from './assets/watsaap.png'; // Your WhatsApp icon
-import emailIcon from './assets/mail.webp'; // Email icon (example)
-import instaIcon from './assets/insta.webp'; // Instagram icon (example)
-
 function App() {
   const [showPopup, setShowPopup] = useState(false);
   const [name, setName] = useState('');
@@ -54,11 +56,8 @@ function App() {
   const [contactPhone, setContactPhone] = useState('');
   const [contactMessage, setContactMessage] = useState('');
   const [currentHeroImage, setCurrentHeroImage] = useState(0);
-  
   const heroImages = [destination7, heroImage2, heroImage3, heroImage4, heroImage5];
-  
 
- 
  
 const settings = {
   dots: true,
@@ -69,26 +68,44 @@ const settings = {
   autoplay: true,
   autoplaySpeed: 3000,  // Speed of slide transition in milliseconds
   pauseOnHover: false,
+  responsive: [
+    {
+      breakpoint: 768, // For tablets
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
 };
 
   
   const destinations = [
-    { id: 1, image: destination1, alt: "Baku", location: "Baku" ,loading:"lazy"},
-    { id: 2, image: destination2, alt: "Bali", location: "Bali" ,loading:"lazy"},
-    { id: 3, image: destination3, alt: "Thailand", location: "Thailand",loading:"lazy" },
-    { id: 4, image: destination4, alt: "Goa", location: "Goa",loading:"lazy" },
-    { id: 5, image: destination5, alt: "Dubai", location: "Dubai",loading:"lazy" },
-    { id: 6, image: destination6, alt: "Rajasthan", location: "Rajasthan",loading:"lazy" },
-    { id: 7, image: destination7, alt: "Srinagar", location: "Srinagar",loading:"lazy" },
-    { id: 8, image: destination8, alt: "Thailand", location: "Thailand",loading:"lazy" },
-    { id: 9, image: destination9, alt: "Singapore", location: "Singapore",loading:"lazy" },
-    { id: 10, image: destination10, alt: "Vietnam", location: "Vietnam",loading:"lazy" },
-    { id: 11, image: destination11, alt: "Odisha", location: "Odisha",loading:"lazy" },
-    { id: 12, image: destination12, alt: "Andaman", location: "Andaman",loading:"lazy" },
-    { id: 13, image: destination13, alt: "Kerala", location: "Kerala",loading:"lazy" }
+    { id: 1, image: destination1, alt: "Baku", location: "Baku" },
+    { id: 2, image: destination2, alt: "Bali", location: "Bali"},
+    { id: 3, image: destination3, alt: "Thailand", location: "Thailand" },
+    { id: 4, image: destination4, alt: "Goa", location: "Goa" },
+    { id: 5, image: destination5, alt: "Dubai", location: "Dubai" },
+    { id: 6, image: destination6, alt: "Rajasthan", location: "Rajasthan"},
+    { id: 7, image: destination7, alt: "Srinagar", location: "Srinagar"  },
+    { id: 8, image: destination8, alt: "Thailand", location: "Thailand"},
+    { id: 9, image: destination9, alt: "Singapore", location: "Singapore"},
+    { id: 10, image: destination10, alt: "Vietnam", location: "Vietnam" },
+    { id: 11, image: destination11, alt: "Odisha", location: "Odisha" },
+    { id: 12, image: destination12, alt: "Andaman", location: "Andaman"},
+    { id: 13, image: destination13, alt: "Kerala", location: "Kerala" }
   ];
 
 
+  
+  // Change the Hero section background image every 5 seconds
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentHeroImage(prev => (prev + 1) % heroImages.length);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
   
   
   // Show popup after 8 seconds
@@ -100,14 +117,6 @@ const settings = {
     return () => clearTimeout(timer);
   }, []);
 
-  // Change the Hero section background image every 5 seconds
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentHeroImage(prev => (prev + 1) % heroImages.length);
-    }, 5000);
-
-    return () => clearInterval(interval);
-  }, []);
 
   // Handle form submission for the popup form
   const handleSubmit = (e) => {
@@ -119,6 +128,11 @@ const settings = {
     } else {
       alert("Please fill in both fields.");
     }
+  };
+
+  // Handle closing the popup
+  const handleClosePopup = () => {
+    setShowPopup(false);
   };
 
   // Handle form submission for the contact form (booking data)
@@ -135,13 +149,9 @@ const settings = {
     }
   };
 
-  // Handle closing the popup
-  const handleClosePopup = () => {
-    setShowPopup(false);
-  };
+  
 
  
-
   return (
     <div className="App">
       {/* New Navbar with Contact Information */}
@@ -220,17 +230,17 @@ const settings = {
    
     <div className="service">
       <img src={service1 }alt="Flight Tickets" className="service-image" loading="lazy" />
-      <h3>Flight Tickets</h3>
+      <h3>Flight Tickets Booking</h3>
       <p>We provide the best deals on flight tickets, ensuring you travel with comfort and convenience at the best prices.</p>
     </div>
 
     <div className="service">
-      <img src={service2 }alt="Train Tickets & Cab/Bus Booking" className="service-image" loading="lazy" />
+      <img src={service2 }alt="Train Tickets " className="service-image" loading="lazy" />
       <h3>Train Tickets Booking</h3>
       <p>Book train tickets seamlessly through our platform for a comfortable journey.</p>
     </div>
     <div className="service">
-      <img src={service3 }alt="Train Tickets & Cab/Bus Booking" className="service-image"loading="lazy" />
+      <img src={service3 }alt=" Cab/Bus Booking" className="service-image"loading="lazy" />
       <h3> Cab/Bus Booking</h3>
       <p>Book cabs, and buses seamlessly through our platform for a comfortable journey.</p>
     </div>
@@ -389,32 +399,7 @@ const settings = {
         </div>
       </section>
 
-      {/* Popup with Form */}
-      {showPopup && (
-        <div className="popup-overlay">
-          <div className="popup">
-            <button style={{backgroundColor:"transparent", color:"black"}} className="close-btn" onClick={handleClosePopup}>&#10006;</button>
-            <h3>Enter your details</h3>
-            <form onSubmit={handleSubmit}>
-              <input 
-                type="text" 
-                placeholder="Name" 
-                value={name} 
-                onChange={(e) => setName(e.target.value)} 
-                required 
-              />
-              <input 
-                type="number" 
-                placeholder="Phone Number" 
-                value={phone} 
-                onChange={(e) => setPhone(e.target.value)} 
-                required 
-              />
-              <button type="submit" className="btn">Submit</button>
-            </form>
-          </div>
-        </div>
-      )}
+      
 
       {/* Footer */}
       
@@ -551,8 +536,38 @@ const settings = {
           <img src={whatsappIcon} alt="Chat on WhatsApp" />
         </a>
       </div>
+
+      {/* Popup with Form */}
+      {showPopup && (
+        <div className="popup-overlay">
+          <div className="popup">
+            <button style={{backgroundColor:"transparent", color:"black"}} className="close-btn" onClick={handleClosePopup}>&#10006;</button>
+            <h3>Enter your details</h3>
+            <form onSubmit={handleSubmit}>
+              <input 
+                type="text" 
+                placeholder="Name" 
+                value={name} 
+                onChange={(e) => setName(e.target.value)} 
+                required 
+              />
+              <input 
+                type="number" 
+                placeholder="Phone Number" 
+                value={phone} 
+                onChange={(e) => setPhone(e.target.value)} 
+                required 
+              />
+              <button type="submit" className="btn">Submit</button>
+            </form>
+          </div>
+        </div>
+      )}
     </div>
+
+    
   );
+  
 }
 
 export default App;
